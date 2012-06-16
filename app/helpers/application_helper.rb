51749f0483dc33153ac7243dc0b941360f3082c3
s12chung <main@mba-steve.local>
2012-06-16 13:59:54 -0400

    homepage + bootstrap integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def login_url
+    "https://www.facebook.com/dialog/oauth?"
+    + "client_id=383285121729602"
+    + "&redirect_uri=YOUR_REDIRECT_URI"
+    + "&scope=COMMA_SEPARATED_LIST_OF_PERMISSION_NAMES"
+    + "&state=SOME_ARBITRARY_BUT_UNIQUE_STRING"
+  end
 end
