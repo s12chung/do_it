7235e3e0b9a189c6e70c4ee2d8e0dc236e5a0a4b
s12chung <main@mba-steve.local>
2012-06-16 12:54:46 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoIt::Application.config.secret_token = '3eae7323cee1a393656bb8fdc240cecc3a8bd8ef05bf1cb28b37e8ae3941f5d7a66e317ddc1b3760797648e4b5a704120407cc1b08447ea26017b9a31ee9fdb1'
